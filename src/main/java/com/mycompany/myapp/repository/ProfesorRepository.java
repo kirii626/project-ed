73d3b0282da718f1042f63fecd6930303060bac8
Alejandra K. Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2024-11-04 16:30:52 -0300

    cambio de tipo de dato

--- src/main/java/com/mycompany/myapp/repository/ProfesorRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ProfesorRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {}
+public interface ProfesorRepository extends JpaRepository<Profesor, String> {}
